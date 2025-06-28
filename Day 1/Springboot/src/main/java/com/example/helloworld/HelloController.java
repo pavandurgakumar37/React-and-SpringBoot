3c43d9d29a1b4feb1d9c4c4a66272d5e5f12d303
Gunti Pavan Durga Kumar <pavandurgakumargunti@gmail.com>
2025-06-28 17:06:36 +0530

    Day 1.1

--- Day 1/Springboot/src/main/java/com/example/helloworld/HelloController.java
+++ Day 1/Springboot/src/main/java/com/example/helloworld/HelloController.java
@@ -1,12 +1,14 @@
 package com.example.helloworld;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")  // Allow frontend origin
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping("/message")
     public String sayHello() {
         return "Hello, Spring Boot!";
     }
